609e1a9e60131b0fb713989012a219ea4edb5e5c
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-09-10 17:13:06 +0700

    Создание библиотек и классов

--- SalesApp/Program.cs
+++ SalesApp/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
+using CLI;
+using SalesLib;
+
 
 namespace SalesApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var db = new DataBase();
+            var products = db.GetProducts();
+            var buyers = db.GetBuyers();
+            Buyer buyer;
+
+            foreach (var item in buyers)
+            {
+                Show.PrintLn($"{item.Id} - {item.Name}");
+            }
+            Show.PrintLn("Выберите номер покупателя");
+            var buyer_id = uint.Parse(Console.ReadLine());
+            if (buyer_id == 0)
+            {
+                buyer = new Buyer();
+            }
+            else
+            {
+                buyer = buyers[(int)(buyer_id - 1)];
+            }
+
+            Show.PrintLn($"{buyer.Name} - {buyer.Discount}");
+
+            foreach (var product in products)
+            {
+                Show.PrintLn($"{product.Id}: {product.Name}, {product.Price}");
+            }
+
+            Show.Print("Введите номер продукта: ");
+            var product_id = uint.Parse(Console.ReadLine());
+            Show.Print("Введите количество: ");
+            var count_user = uint.Parse(Console.ReadLine());
+
+            var count_stock = db.GetProductCount(product_id);
+
+            if (count_user > count_stock)
+            {
+                Show.Error("Столько нет товара на складе");
+                return;
+            }
+
+            var price = products[(int)(product_id - 1)].Price;
+            var total_price = count_user * (price - price * buyer.Discount / 100);
+            Show.PrintLn($"Вам необходимо заплатить - {total_price}");
         }
     }
 }
+    
+
